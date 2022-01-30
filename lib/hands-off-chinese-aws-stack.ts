e5001d2a887a404e131866a06ef801282bbf9345
Jakob Willforss <jakob.willforss@hotmail.com>
2022-01-30 18:12:00 +0100

    Setup minimal lambda function

--- lib/hands-off-chinese-aws-stack.ts
+++ lib/hands-off-chinese-aws-stack.ts
@@ -5,21 +5,32 @@
 import * as cdk from "aws-cdk-lib";
 import { aws_s3 as s3 } from "aws-cdk-lib";
 
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+
+// export class HandsOffChineseAwsStack extends cdk.Stack {
+//   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     new s3.Bucket(this, "MyFirstBucket", {
+//       versioned: true,
+//       removalPolicy: cdk.RemovalPolicy.DESTROY,
+//       autoDeleteObjects: true
+//     });
+
+//     // The code that defines your stack goes here
+
+//     // example resource
+//     // const queue = new sqs.Queue(this, 'HandsOffChineseAwsQueue', {
+//     //   visibilityTimeout: cdk.Duration.seconds(300)
+//     // });
+//   }
+// }
+
 export class HandsOffChineseAwsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "MyFirstBucket", {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
-    });
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HandsOffChineseAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
