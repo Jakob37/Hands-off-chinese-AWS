88bae151fce9d3b8664d16e1c4fca6a1443cee5e
Jakob Willforss <jakob.willforss@hotmail.com>
2022-02-09 20:39:11 +0100

    Minimal dynamo entry put

--- lambda/writetodynamo.js
+++ lambda/writetodynamo.js
@@ -1,29 +1,35 @@
 const AWS = require("aws-sdk");
 
 exports.handler = async function (event) {
-
     var body = JSON.parse(event.body);
 
     const dynamo = new AWS.DynamoDB();
 
+    let result;
     await dynamo
-        .updateItem({
-            TableName: process.env.TABLE_NAME,
-            Key: {
-                id: { S: body.id },
-                text: { S: body.content },
-                // filename: event.filename,
-                // creationdate: event.creationdate,
-                // category: event.category,
-                // language: event.language,
-                // status: 'active',
+        .putItem(
+            {
+                TableName: process.env.TABLE_NAME,
+                Item: {
+                    id: { S: body.id },
+                    text: { S: body.content },
+                    // filename: event.filename,
+                    // creationdate: event.creationdate,
+                    // category: event.category,
+                    // language: event.language,
+                    // status: 'active',
+                },
             }
-        })
+        )
         .promise();
 
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Successfully put event: ${JSON.stringify(event, null, 2)}`
-    }
-}
+        body: `Successfully put event: ${JSON.stringify(
+            event,
+            null,
+            2
+        )}`,
+    };
+};
