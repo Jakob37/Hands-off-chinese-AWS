c693bc7ba41ebf211432a6ef5b69a0074284a224
Jakob Willforss <jakob.willforss@hotmail.com>
2022-03-31 21:49:37 +0200

    Drafting running post command to new API

--- lambda/writeuserdatatodynamo.js
+++ lambda/writeuserdatatodynamo.js
@@ -27,3 +27,20 @@ exports.handler = async function (event) {
         )}`,
     };
 };
+
+// READ:
+// API Gateway elements
+// https://www.alexdebrie.com/posts/api-gateway-elements/
+
+// Running through the manual direct test
+// {"body": "{\"id\":\"myid\", \"user\":\"myuser\", \"jsonString\":\"test\"}"}
+
+// Running through the Gateway
+// {"id":"myid", "user":"myuser", "jsonString":"test"}
+
+// Running through axios put
+// axios2
+// .post("https://<URL>.execute-api.eu-west-1.amazonaws.com/prod/userdata", 
+// {"id":"myid", "user":"myuser", "jsonString":"test"})
+// .then(function(response){ console.log('response', response) })
+// .catch(function(error) { if (error.response) {console.log(error.response.data)} else {console.log(error)} })
